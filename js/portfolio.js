90c24a8fd3b04e7103443f63c2f11c2c4295e3e6
Todd Austin <toddaustin33@gmail.com>
2017-11-15 15:10:38 -0700

    add portfolio files

---
+++ js/portfolio.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    // var navigation = Array.from(document.querySelectorAll('nav a'));
+    // for (var i = navigation.length - 1; i >= 0; i--) {
+    //     navigation[i].addEventListener('click', function(){
+    //         var currentView = '#' + $(this).attr('class').split(' ')[0];
+    //         $('nav a').removeClass('active');
+    //         $(this).addClass('active');
+    //         $(currentView).scrollintoview({
+    //             duration: 1000,
+    //             direction: "vertical"
+    //         });
+    //     });
+    // }
+});
+
+
