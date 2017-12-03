9201912081d044198dce7b735c570683a64dde10
Todd <toddaustin33@gmail.com>
2017-12-03 11:04:28 -0700

    update interactivity

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,41 @@
 function ready(a){"loading"!=document.readyState?a():document.addEventListener("DOMContentLoaded",a)}
-
+var nav;
 ready(function(){
     var about = document.querySelector('.one');
     var projects = document.querySelector('.two');
     var skill = document.querySelector('.three');
     var contact = document.querySelector('.four');
 
-    var nav = document.querySelectorAll('nav ul li a');
+     nav = document.querySelectorAll('nav ul li a');
     
     for (var i = nav.length - 1; i >= 0; i--) {
-        console.log(nav[i].className);
-        // nav[i].addEventListener("click",function(){
-        //     console.log(nav[i]);
-        //    currentNav = this.className;
-        //    console.log(className);
-        // });
+     
+        nav[i].addEventListener("click",function(){
+          hideAll();
+
+           if(this.classList.contains('about')){
+                about.classList.remove('hidden');
+                this.classList.add('active');
+           } else if(this.classList.contains('projects')){
+                projects.classList.remove('hidden');
+                this.classList.add('active');
+           }
+        });
     }
 
 });
 
+function removeActive() {
+    for (var i = nav.length - 1; i >= 0; i--) {
+        nav[i].classList.remove('active');
+    }
+}
 
+function hideAll() {
+   
+    var sections = document.querySelectorAll('section');
+    for (var i = sections.length - 1; i >= 0; i--) {
+        sections[i].classList.add('hidden');
+    }
+    removeActive();
+}
