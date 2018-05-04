14f1a652185fac853827132e093911c0b3d5b528
Todd <toddaustin33@gmail.com>
2018-05-03 18:40:49 -0700

    update all

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,29 +1,25 @@
 function ready(a){"loading"!=document.readyState?a():document.addEventListener("DOMContentLoaded",a)}
 var nav;
 ready(function(){
-    var about = document.querySelector('.one');
-    var projects = document.querySelector('.two');
-    var skill = document.querySelector('.three');
-    var contact = document.querySelector('.four');
+    // var about = document.querySelector('.one');
+    // var projects = document.querySelector('.two');
+    // var skills = document.querySelector('.three');
+    // var contact = document.querySelector('.four');
 
-     nav = document.querySelectorAll('nav ul li a');
+    nav = document.querySelectorAll('nav ul li a');
 
-     showProject();
+    showProject();
     
     for (var i = nav.length - 1; i >= 0; i--) {
-     
         nav[i].addEventListener("click",function(){
-          hideAll();
-           if(this.classList.contains('about')){https://smile.amazon.com/Barbell-Deluxe-Power-Rack-FM-CB8000F/dp/B00I04Z52G/ref=lp_17372558011_1_5?s=sports-and-fitness&ie=UTF8&qid=1512353539&sr=1-5#
-                about.classList.remove('hidden');
+            hideAll();
+            var current = this.textContent.toLowerCase();
+            if(this.classList.contains(current)){
                 this.classList.add('active');
-           } else if(this.classList.contains('projects')){
-                projects.classList.remove('hidden');
-                this.classList.add('active');
-           }
+                document.querySelector("#" + current).classList.remove('hidden');
+           } 
         });
     }
-
 });
 
 function removeActive() {
@@ -33,9 +29,8 @@ function removeActive() {
 }
 
 function hideAll() {
-   
     var sections = document.querySelectorAll('section');
-    for (var i = sections.length - 1; i >= 0; i--) {
+    for (var i = 0; i <sections.length;i++) {
         sections[i].classList.add('hidden');
     }
     removeActive();
@@ -43,10 +38,13 @@ function hideAll() {
 
 function showProject() {
     var projectContainer = document.querySelectorAll('.project-tabs li');
-
     var proj = document.querySelectorAll('.proj');
     for (var i = projectContainer.length - 1; i >= 0; i--) {
         projectContainer[i].addEventListener("click",function(){ 
+            for (var j = proj.length - 1; j >= 0; j--) {
+                proj[j].classList.add('hidden');
+            }
+   
             var curClass = this.className;   
             var currentProj = document.querySelector('.' + curClass + '-content');
             currentProj.classList.remove('hidden');
