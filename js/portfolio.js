a9b1f6e37e0de6eadfbc737fe5f36aa93ed1a1b9
Todd <toddaustin33@gmail.com>
2018-05-03 19:17:25 -0700

    updates

--- js/portfolio.js
+++ js/portfolio.js
@@ -20,6 +20,8 @@ ready(function(){
            } 
         });
     }
+    var years = document.querySelector('.years');
+    years.textContent = calculateYears();
 });
 
 function removeActive() {
@@ -40,14 +42,26 @@ function showProject() {
     var projectContainer = document.querySelectorAll('.project-tabs li');
     var proj = document.querySelectorAll('.proj');
     for (var i = projectContainer.length - 1; i >= 0; i--) {
+    
         projectContainer[i].addEventListener("click",function(){ 
             for (var j = proj.length - 1; j >= 0; j--) {
                 proj[j].classList.add('hidden');
             }
-   
+            [].forEach.call(projectContainer, function(e) {
+                e.classList.remove("active-tab");
+            });
             var curClass = this.className;   
             var currentProj = document.querySelector('.' + curClass + '-content');
+            this.classList.add('active-tab');
             currentProj.classList.remove('hidden');
         });
     }
 }
+
+
+    function calculateYears() { // birthday is a date
+        var started= new Date(1998, 1, 10);
+       var ageDifMs = Date.now() - started.getTime();
+       var ageDate = new Date(ageDifMs); // miliseconds from epoch
+       return Math.abs(ageDate.getUTCFullYear() - 1970);
+     }
